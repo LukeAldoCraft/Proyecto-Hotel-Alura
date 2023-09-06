7ec3c54de64448caaf6c7ff0e85067191599c37f
AldoRJ <91865442+LukeAldoCraft@users.noreply.github.com>
2023-09-06 17:57:45 -0600

    feat

--- src/main/java/com/mx/alura/hotel/utils/JPAUtils.java
+++ src/main/java/com/mx/alura/hotel/utils/JPAUtils.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class JPAUtils {
      
-	private static EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("hotel");
+	private static EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("Hotel");
 	
 	
 	public static EntityManager getEntityManager() {
